ff0cd9333eca340a23953c109c45e8747b25d3cd
Yin Heng <18792544700@163.com>
2019-01-28 02:53:04 +0800

    debug

--- api.py
+++ api.py
@@ -4,12 +4,12 @@ from mongo import MongoDB
 app = Flask(__name__)
 
 class API:
-	def __init__(self):
-		self.rankdb = MongoDB('Javdb', 'rankdb')
+    def __init__(self):
+        self.rankdb = MongoDB('Javdb', 'rankdb')
 
-	@app.route('/<identity>', methods=['GET', 'POST'])
-	def identity(identity = None):
-	    return self.rankdb.find({'identity': identity})
+    @app.route('/<identity>', methods=['GET', 'POST'])
+    def identity(self, identity = None):
+        return str(self.rankdb.find({'identity': identity}))
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port = 8080, threaded=True)
+    app.run(host='0.0.0.0', port = 8080, threaded=True)
